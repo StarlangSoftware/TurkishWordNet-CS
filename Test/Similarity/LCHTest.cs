010f60879daee00bf9ec6ff43c4d761989ec6803
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-09-30 19:11:52 +0300

    Updated wordnet. Removed multiple hypernyms.

--- Test/Similarity/LCHTest.cs
+++ Test/Similarity/LCHTest.cs
@@ -13,12 +13,6 @@ namespace Test.Similarity
             Assert.AreEqual(2.8332,
                 lch.ComputeSimilarity(turkish.GetSynSetWithId("TUR10-0656390"),
                     turkish.GetSynSetWithId("TUR10-0600460")), 0.0001);
-            Assert.AreEqual(0.2623,
-                lch.ComputeSimilarity(turkish.GetSynSetWithId("TUR10-0066050"),
-                    turkish.GetSynSetWithId("TUR10-1198750")), 0.0001);
-            Assert.AreEqual(0.5596,
-                lch.ComputeSimilarity(turkish.GetSynSetWithId("TUR10-0012910"),
-                    turkish.GetSynSetWithId("TUR10-0172740")), 0.0001);
             Assert.AreEqual(0.7673,
                 lch.ComputeSimilarity(turkish.GetSynSetWithId("TUR10-0412120"),
                     turkish.GetSynSetWithId("TUR10-0755370")), 0.0001);
--- Test/Similarity/WuPalmerTest.cs
+++ Test/Similarity/WuPalmerTest.cs
@@ -13,12 +13,6 @@ namespace Test.Similarity
             Assert.AreEqual(0.9697,
                 lch.ComputeSimilarity(turkish.GetSynSetWithId("TUR10-0656390"),
                     turkish.GetSynSetWithId("TUR10-0600460")), 0.0001);
-            Assert.AreEqual(0.1739,
-                lch.ComputeSimilarity(turkish.GetSynSetWithId("TUR10-0066050"),
-                    turkish.GetSynSetWithId("TUR10-1198750")), 0.0001);
-            Assert.AreEqual(0.3636,
-                lch.ComputeSimilarity(turkish.GetSynSetWithId("TUR10-0012910"),
-                    turkish.GetSynSetWithId("TUR10-0172740")), 0.0001);
             Assert.AreEqual(0.25,
                 lch.ComputeSimilarity(turkish.GetSynSetWithId("TUR10-0412120"),
                     turkish.GetSynSetWithId("TUR10-0755370")), 0.0001);
